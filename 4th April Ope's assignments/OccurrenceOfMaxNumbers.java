e10dd65844ee29c0c70b984b2b09de58280a4155
theseven7h <jamestauri123@gmail.com>
2025-04-21 17:05:45 +0100

    Added OccurrenceOfMaxNumbers program

---
+++ 4th April Ope's assignments/OccurrenceOfMaxNumbers.java
@@ -0,0 +1,26 @@
+import java.util.Scanner;
+
+public class OccurrenceOfMaxNumbers {
+	public static void main(String[] args) {
+		Scanner input = new Scanner(System.in);
+
+		System.out.print("Enter numbers: ");
+		int numbers = input.nextInt();
+		int count = 1;
+		int max = -2000000;
+		
+		while (numbers != -1) {
+			numbers = input.nextInt();
+			if (numbers > max) {
+				max = numbers; 
+		 		count = 1;
+			}
+			else if (numbers == max) count++;
+			
+		}
+
+		System.out.println("The largest number is " + max);
+		System.out.print("The occurrence count of the largest number is " + count);
+	}
+}
+		
