3bb793f5a3b947dcf8191344a315b10b89fa974a
theseven7h <jamestauri123@gmail.com>
2025-04-29 12:51:23 +0100

    Face pattern first trial

---
+++ Create/FacePattern.java
@@ -0,0 +1,12 @@
+public class FacePattern {
+	public static void main(String[] args) {
+		System.out.println();
+		for (int y = 1; y <= 5; y++) {
+			if (y == 1) System.out.println("  +\" \" \" \" \"+  ");	
+			if (y == 2) System.out.println("[ |  o   o  | ]");
+			if (y == 3) System.out.println("  |    ^    | ");
+			if (y == 4) System.out.println("  |  ' - '  | ");
+			if (y == 5) System.out.println("  +- - - - -+ ");
+		}
+	}
+}
