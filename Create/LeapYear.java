8976b1c8271822c40c79bdeb301f2370a98ab40e
theseven7h <jamestauri123@gmail.com>
2025-04-29 12:18:17 +0100

    Leap year completed

--- Create/LeapYear.java
+++ Create/LeapYear.java
@@ -8,7 +8,9 @@ public class LeapYear {
 		int year = input.nextInt();
 
 		while (year != 0) {
-			if (year % 4 == 0 || year % 400 == 0) System.out.print(year + " is a leap year\n\n");
+			if ((year % 4 == 0 && year % 100 != 0) || (year % 100 == 0 && year % 400 == 0)) {
+				System.out.print(year + " is a leap year\n\n");
+			}
 			else System.out.print(year + " is not a leap year\n\n");
 			System.out.print("Enter any year: ");
 			year = input.nextInt();
