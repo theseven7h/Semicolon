7d22dc34d5b2aa1a0375043baf33a1d40a5cbde5
theseven7h <jamestauri123@gmail.com>
2025-05-03 18:44:12 +0100

    Binary to decimal finally done

--- Deitel Chapter 4/Exercises/PrintingDecimalOfBinary.java
+++ Deitel Chapter 4/Exercises/PrintingDecimalOfBinary.java
@@ -5,8 +5,17 @@ public class PrintingDecimalOfBinary {
 		Scanner input = new Scanner(System.in);
 
 		System.out.print("Enter a binary number: ");
-		int binary = input.nextInt();
-		
-		for (;num ) {
-
+		int number = input.nextInt();
+		int decimal = 0;
+		int power = 1;
+		System.out.print(number);
+		while (number != 0) {	
+			int binary = number % 10;
+			decimal += (binary * power);
+			power *= 2;
+			number /= 10;
+		}
+		System.out.print(" decimal equivalent is " + decimal);
+	}
+}
 		
