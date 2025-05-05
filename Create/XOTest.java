68e85f5f4069f00ebb05579a3114e394a7a26e93
theseven7h <jamestauri123@gmail.com>
2025-05-05 18:51:41 +0100

    Third approach to initial ArrayXO

---
+++ Create/XOTest.java
@@ -0,0 +1,9 @@
+public class XOTest {
+	public static void main(String[] args) {
+		char[][] xo = {{'X', 'O' , 'X'}, {'O', 'O', 'O'}, {'X', 'X', 'O'}};
+
+		for (int i = 0; i < 3; i++) {
+			System.out.println(xo[i]);
+		}
+	}
+}
