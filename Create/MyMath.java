49d0daf9653b73a33a47fc9f082c92a7207cf330
theseven7h <jamestauri123@gmail.com>
2025-05-11 13:53:14 +0100

    Starting MyMath class for my arthmetics

---
+++ Create/MyMath.java
@@ -0,0 +1,11 @@
+public class MyMath {
+	static int findAdditiveInverse(int number) {
+		number = -number
+		return number;
+	}
+	
+	static double findAdditiveInverse(double number) {
+		number = -number
+		return number;
+	}
+}
