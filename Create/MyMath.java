172d4b1f62635ecda51010842b501a01878c87ae
theseven7h <jamestauri123@gmail.com>
2025-05-11 13:58:23 +0100

    Added additive inverse for arrays

--- Create/MyMath.java
+++ Create/MyMath.java
@@ -8,4 +8,11 @@ public class MyMath {
 		number = -number
 		return number;
 	}
+	
+	static int[] findAdditiveInverse(int[] numbers) {
+		for (int i = 0; i < numbers.length; i++) {
+			numbers[i] = findAdditiveInverse(numbers[i])
+		}
+		return numbers;
+	}
 }
