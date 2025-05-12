ad31c074def50b31c5fb11e45c7513826adce551
theseven7h <jamestauri123@gmail.com>
2025-05-12 02:34:09 +0100

    Header added

--- Chibuzor's Work/MultiplicationTable.java
+++ Chibuzor's Work/MultiplicationTable.java
@@ -1,6 +1,12 @@
 public class MultiplicationTable {
 	static void displayMultiplicationTable(int a, int b) {
+		System.out.print("    ");
+		for (int header = a; header <= b; header++) {
+			System.out.printf("%5s%d%s", "|", header, "|");
+		}
+		System.out.println();
 		for(int row = a; row <= b; row++) {
+			System.out.printf("%d |", row);
 			for(int count = a; count <= b; count++) {
 				System.out.printf("%7d", row * count);
 			}
