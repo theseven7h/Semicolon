f592a094a21f497c707f63d4c9ab4b388481a193
theseven7h <jamestauri123@gmail.com>
2025-05-19 07:26:11 +0100

    Driver class for function categorizeNumbers

---
+++ TDD projects/CategorizeNumbersTest.java
@@ -0,0 +1,8 @@
+public class CategorizeNumbersTest {
+	public static void main(String[] args) {
+		int[] numbers = {3,4,5,6,7};
+		int divisor = 77;
+		
+		CategorizeNumbers.categorizeNumbers(numbers, divisor);
+	}
+}
