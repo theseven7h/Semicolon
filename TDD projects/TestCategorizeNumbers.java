90a1d4af4700fffb49af1ac1381ee78162242d74
theseven7h <jamestauri123@gmail.com>
2025-05-19 05:43:11 +0100

    Test Driven Development for categorizeNmbers function

---
+++ TDD projects/TestCategorizeNumbers.java
@@ -0,0 +1,11 @@
+import org.junit.jupiter.api.Test;
+import static org.junit.jupiter.api.Assertions.*;
+
+public class TestCategorizeNumbers {
+	
+	@Test
+	public void testCategorizeFunctionsExists() {
+		CategorizeNumbers divisor = new CategorizeNumbers(); 
+		divisor.categorizeNumbers();
+	}
+}
