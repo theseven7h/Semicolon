6ac721a5df2c53ee66eb26b6d3238f7a19ea7a69
theseven7h <jamestauri123@gmail.com>
2025-05-28 16:03:14 +0100

    Function and 2 tests for omitting duplicates in an array in progress

--- Phase Gate/Day 2/Duplicate.java
+++ Phase Gate/Day 2/Duplicate.java
@@ -1,5 +1,37 @@
 public class Duplicate {
+	static int noOfUniqueValuesIn(int[] numbers) {
+		int[] values = new int[numbers.length];
+		int count = 0;
+		int uniqueCount = 0;
+		for (int i = 0; i < numbers.length; i++) {
+			count = 0;
+			values[i] = numbers[i];
+
+			for (int j: numbers) {
+				if (j == values[i]) count++;
+			}
+			if (count > 1) uniqueCount++;
+		}
+		return uniqueCount;
+	}
+	
 	static int[] omitDuplicateIn(int[] numbers) {
+		int length = numbers.length - noOfUniqueValuesIn(numbers);
+		int[] values = new int[length];
+		int count = 0;
 		
+		for (int i = 0; i < numbers.length; i++) {
+			
+			for(int j : numbers) {
+				if(j == numbers[i]) {
+					count = 0;
+					for(int k: values) {
+						if (k == j) count++;
+					}
+					if (count == 1) values[i] = j;
+				}
+			}
+		}
+		return values;
 	}
 }
--- Phase Gate/Day 2/TestDuplicate.java
+++ Phase Gate/Day 2/TestDuplicate.java
@@ -4,4 +4,17 @@ import static org.junit.jupiter.api.Assertions.*;
 public class TestDuplicate {
 	Duplicate dup = new Duplicate();
 	
+	@Test
+	void testomitDuplicateInFunctionExists() {
+		int[] numbers = {1,2,3,2};
+		dup.omitDuplicateIn(numbers);
 	}
+	
+	@Test
+	void testomitDuplicateInFunctionReturnsRightSum() {
+		int[] numbers = {1,2,2,3,4,4,5,6,6,7};
+		int[] expected = {1,2,3,4,5,6,7};
+		assertEquals(expected, dup.omitDuplicateIn(numbers));	
+	}
+	
+}
